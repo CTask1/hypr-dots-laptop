2ae8b6c662b0e18f6a29d972f77f660ea20f9681
CTask1 <cjtasker07@gmail.com>
2026-09-11 16:56:20 -0500

    File manager change reverted, further redundancy reduction

--- configs/keybindings.lua
+++ configs/keybindings.lua
@@ -7,7 +7,7 @@
 local mainMod = "SUPER" -- Sets "Windows" key as main modifier
 local vars = {
     terminal = "kitty",
-    fileManager = "thunar",
+    fileManager = "dolphin",
     menu = "rofi -show drun",
     browser = "zen-browser",
     textedit = "code",
@@ -24,8 +24,10 @@ local function binds(kb)
 end
 
 local dsp = hl.dsp
+local focus = dsp.focus
 local cmd = dsp.exec_cmd
 local window = dsp.window
+local move = window.move
 local asink = "@DEFAULT_AUDIO_SINK@"
 local asource = "@DEFAULT_AUDIO_SOURCE@"
 
@@ -46,49 +48,49 @@ binds ({
     {"CTRL + SHIFT + Q", cmd("hyprshutdown")},
     {"L", cmd("hyprlock")},
     -- Move focus with mainMod + arrow keys
-    {"left", dsp.focus({ direction = "left" })},
-    {"right", dsp.focus({ direction = "right" })},
-    {"up", dsp.focus({ direction = "up" })},
-    {"down", dsp.focus({ direction = "down" })},
+    {"left", focus({ direction = "left" })},
+    {"right", focus({ direction = "right" })},
+    {"up", focus({ direction = "up" })},
+    {"down", focus({ direction = "down" })},
     -- Move active window with mainMod + Shift + arrow keys
-    {"SHIFT + left", window.move({ direction = "left" })},
-    {"SHIFT + right", window.move({ direction = "right" })},
-    {"SHIFT + up", window.move({ direction = "up" })},
-    {"SHIFT + down", window.move({ direction = "down" })},
+    {"SHIFT + left", move({ direction = "left" })},
+    {"SHIFT + right", move({ direction = "right" })},
+    {"SHIFT + up", move({ direction = "up" })},
+    {"SHIFT + down", move({ direction = "down" })},
     -- Switch workspaces with mainMod + [0-9]
-    {"1", dsp.focus({ workspace = 1 })},
-    {"2", dsp.focus({ workspace = 2 })},
-    {"3", dsp.focus({ workspace = 3 })},
-    {"4", dsp.focus({ workspace = 4 })},
-    {"5", dsp.focus({ workspace = 5 })},
-    {"6", dsp.focus({ workspace = 6 })},
-    {"7", dsp.focus({ workspace = 7 })},
-    {"8", dsp.focus({ workspace = 8 })},
-    {"9", dsp.focus({ workspace = 9 })},
-    {"0", dsp.focus({ workspace = 10 })},
+    {"1", focus({ workspace = 1 })},
+    {"2", focus({ workspace = 2 })},
+    {"3", focus({ workspace = 3 })},
+    {"4", focus({ workspace = 4 })},
+    {"5", focus({ workspace = 5 })},
+    {"6", focus({ workspace = 6 })},
+    {"7", focus({ workspace = 7 })},
+    {"8", focus({ workspace = 8 })},
+    {"9", focus({ workspace = 9 })},
+    {"0", focus({ workspace = 10 })},
     -- Move active window to a workspace with mainMod + SHIFT + [0-9]
-    {"SHIFT + 1", window.move({ workspace = 1 })},
-    {"SHIFT + 2", window.move({ workspace = 2 })},
-    {"SHIFT + 3", window.move({ workspace = 3 })},
-    {"SHIFT + 4", window.move({ workspace = 4 })},
-    {"SHIFT + 5", window.move({ workspace = 5 })},
-    {"SHIFT + 6", window.move({ workspace = 6 })},
-    {"SHIFT + 7", window.move({ workspace = 7 })},
-    {"SHIFT + 8", window.move({ workspace = 8 })},
-    {"SHIFT + 9", window.move({ workspace = 9 })},
-    {"SHIFT + 0", window.move({ workspace = 10 })},
+    {"SHIFT + 1", move({ workspace = 1 })},
+    {"SHIFT + 2", move({ workspace = 2 })},
+    {"SHIFT + 3", move({ workspace = 3 })},
+    {"SHIFT + 4", move({ workspace = 4 })},
+    {"SHIFT + 5", move({ workspace = 5 })},
+    {"SHIFT + 6", move({ workspace = 6 })},
+    {"SHIFT + 7", move({ workspace = 7 })},
+    {"SHIFT + 8", move({ workspace = 8 })},
+    {"SHIFT + 9", move({ workspace = 9 })},
+    {"SHIFT + 0", move({ workspace = 10 })},
     -- Special workspaces
     {"S", dsp.workspace.toggle_special("term")},
-    {"SHIFT + S", window.move({ workspace = "special:term" })},
+    {"SHIFT + S", move({ workspace = "special:term" })},
     {"X", dsp.workspace.toggle_special("work1")},
-    {"SHIFT + X", window.move({ workspace = "special:work1" })},
+    {"SHIFT + X", move({ workspace = "special:work1" })},
     {"Z", dsp.workspace.toggle_special("work2")},
-    {"SHIFT + Z", window.move({ workspace = "special:work2" })},
+    {"SHIFT + Z", move({ workspace = "special:work2" })},
     {"A", dsp.workspace.toggle_special("work3")},
-    {"SHIFT + A", window.move({ workspace = "special:work3" })},
+    {"SHIFT + A", move({ workspace = "special:work3" })},
     -- Scroll through existing workspaces with mainMod + scroll
-    {"mouse_down", dsp.focus({ workspace = "e+1" })},
-    {"mouse_up", dsp.focus({ workspace = "e-1" })},
+    {"mouse_down", focus({ workspace = "e+1" })},
+    {"mouse_up", focus({ workspace = "e-1" })},
     -- Move/resize windows with mainMod + LMB/RMB and dragging
     {"mouse:272", window.drag()},
     {"mouse:273", window.resize()},
